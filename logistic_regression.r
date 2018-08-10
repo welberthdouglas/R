a97a57898299689237794d758fa04a162d15762c
Welberth Douglas <42102076+welberthdouglas@users.noreply.github.com>
2018-08-10 14:21:50 -0300

    Update logistic_regression.r

--- logistic_regression.r
+++ logistic_regression.r
@@ -26,3 +26,26 @@ attach(Smarket)   #attaching the dataframe to make variables available by name
 
 table(glm.pred,Direction)  
 
+mean(glm.pred==Direction)                  # Calculates the total error rate of the model ( times when glm.pred is equal to Direction/ total # of observations)
+
+
+# Dividing the data into training and test sets
+
+trainv=Year<2005                           # makes a subset of the dataframe containing only data prior to 2005
+
+# Fitting the data using only the training set
+
+glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,data=Smarket,family=binomial,subser=trainv)
+#      !!!!!     See if works with update
+#glm2.fit=update(glm.fit,data=Smarket,subset=trainv)
+
+# predicting for the test set
+
+glm.probs=predict(glm.fit,newdata=Smarket[!trainv],type="response")
+glm.pred=ifelse(glm.probs>0.5,"UP","DOWN")
+Direction.2005=Smarket$Direction[!trainv]   # creating a subset of data for the test data
+
+table(glm.pred,Direction.2005)  
+mean(glm.pred==Direction.2005) 
+
+
