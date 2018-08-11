18fb26ab70f34c538f708e15495dcb4247bf7373
Welberth Douglas <42102076+welberthdouglas@users.noreply.github.com>
2018-08-11 00:30:52 -0300

    Update logistic_regression.r

--- logistic_regression.r
+++ logistic_regression.r
@@ -14,12 +14,12 @@ glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,data=Smarket,family=binomi
 
 glm.probs=predict(glm.fit,type="response")  #Predicts the response  
 glm.probs[1:5]                              #shows the first 5 preditions for the data used to fit the model
-                                            #logistic regression returns probabilities, in that case all are close to 0.5%
-                                            #which is reasonable since de data consists of stock market data
+#logistic regression returns probabilities, in that case all are close to 0.5%
+#which is reasonable since de data consists of stock market data
 
 # setting a threshold for the classificator using the ifelse command
 
-glm.pred=ifelse(glm.probs>0.5,"UP","DOWN")
+glm.pred=ifelse(glm.probs>0.5,"Up","Down")
 attach(Smarket)   #attaching the dataframe to make variables available by name
 
 # CREATING A CONFUSION MATRIX
@@ -35,17 +35,16 @@ trainv=Year<2005                           # makes a subset of the dataframe con
 
 # Fitting the data using only the training set
 
-glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,data=Smarket,family=binomial,subser=trainv)
-#      !!!!!     See if works with update
-#glm2.fit=update(glm.fit,data=Smarket,subset=trainv)
+#glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,data=Smarket,family=binomial,subser=trainv)
+#      
+glm.fit=update(glm.fit,data=Smarket,subset=trainv)
 
 # predicting for the test set
 
-glm.probs=predict(glm.fit,newdata=Smarket[!trainv],type="response")
-glm.pred=ifelse(glm.probs>0.5,"UP","DOWN")
+glm.probs=predict(glm.fit,newdata=Smarket[!trainv,],type="response")
+glm.pred=ifelse(glm.probs>0.5,"Up","Down")
 Direction.2005=Smarket$Direction[!trainv]   # creating a subset of data for the test data
 
 table(glm.pred,Direction.2005)  
 mean(glm.pred==Direction.2005) 
-
 
