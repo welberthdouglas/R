dc4e6c3b4fb547dde5311f9d0032bc2b30bcb160
Welberth Douglas <42102076+welberthdouglas@users.noreply.github.com>
2018-08-11 01:11:00 -0300

    Update LDA.r

--- LDA.r
+++ LDA.r
@@ -1,4 +1,20 @@
-
 #------------------------------------------------------------------#
 #               LDA - Linear Discriminant Analysis                 #
 #__________________________________________________________________#
+
+require(ISLR)
+require(MASS)
+
+lda.fit=lda(Direction~Lag1+Lag2,data=Smarket, subset=Year<2005)
+lda.fit
+plot(lda.fit)
+Smarket.2005=subset(Smarket,Year==2005)
+lda.pred=predict(lda.fit,Smarket.2005)
+
+class(lda.pred)
+data.frame(lda.pred)[1:5,]
+table(lda.pred$class,Smarket.2005$Direction)
+mean(lda.pred$class==Smarket.2005$Direction)
+
+
+# add comments!!
